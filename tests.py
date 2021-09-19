751f2481f55b235891ab450afd7f0375b79d0bbe
Hype3808 <74125180+Hype3808@users.noreply.github.com>
2021-09-19 17:24:26 +0800

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,7 +5,7 @@ import cse
 
 class TestMethods(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(cse.smile(), ":)")
+        self.assertEqual(cse)
 
 
 if __name__ == '__main__':
